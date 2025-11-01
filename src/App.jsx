cc517da96d29fe1554b4866570d80ce946c01558
Quynhnhu0203 <163087967+Quynhnhu0203@users.noreply.github.com>
2025-11-01 16:04:41 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SumCalculator from "./components/SumCalculator";
+
+function App() {
+  return (
+    <div className="app">
+      <SumCalculator />
+    </div>
+  );
+}
+
+export default App;
